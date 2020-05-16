dcafd18d25d4d4ccd040943e3f215f45baa5c764
Edwin Sirko <edsirko@microsoft.com>
2020-05-16 17:59:58 -0400

    initial avalonia project

---
+++ egit/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace egit.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
