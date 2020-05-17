4357bc3c1cb12c52fa5e353d740b2bab8bd46317
Edwin Sirko <edsirko@microsoft.com>
2020-05-16 21:24:12 -0400

    View_RepoInfo

---
+++ egit/Views/View_RepoInfo.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace egit.Views
+{
+    public class View_RepoInfo : UserControl
+    {
+        public View_RepoInfo()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
