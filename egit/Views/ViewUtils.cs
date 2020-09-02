8ad3c1fa6bb7589eea58f060a98e6a261cc050f4
Edwin Sirko <esirko@github.com>
2020-09-02 16:26:59 -0400

    colored changelists (#3)
    * colored changelists
    
    * rightclick implementation for file list

---
+++ egit/Views/ViewUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace egit.Views
+{
+    class ViewUtils
+    {
+        static List<IBrush> ChangelistColors = new List<IBrush> { Brushes.Pink, Brushes.SkyBlue, Brushes.LightGreen, Brushes.Yellow, Brushes.Orange,
+            Brushes.PaleVioletRed, Brushes.Cyan, Brushes.Magenta, Brushes.Lime,
+            Brushes.MediumVioletRed, Brushes.Teal, Brushes.Lavender,
+            Brushes.SandyBrown, Brushes.Beige,
+            Brushes.Sienna, Brushes.MintCream, Brushes.Olive, Brushes.NavajoWhite,
+            Brushes.AliceBlue,
+            Brushes.Gray};
+
+        public static void ColorRowByChangelist(DataGridRow row, int changelistIndex)
+        {
+            if (changelistIndex > 0)
+            {
+                row.Background = ChangelistColors[(changelistIndex - 1) % ChangelistColors.Count];
+            }
+        }
+
+    }
+}
